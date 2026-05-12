32f053fd9252e455e7469e72f2fcc2bb9d41ec12
Clara Vas <vasconcelosusa2508@gmail.com>
2026-05-12 17:18:42 -0300

    mudanças no eixo y

---
+++ EstudosPy/INTERFACES/onda.py
@@ -0,0 +1,76 @@
+import pyaudio
+import os
+import struct
+import numpy as np
+import matplotlib.pyplot 
+import matplotlib.pyplot as plt
+import time
+from tkinter import TclError
+
+matplotlib.use('TkAgg')
+
+# constants
+CHUNK = 1024 * 2             # samples per frame
+FORMAT = pyaudio.paInt16     # audio format (bytes per sample?)
+CHANNELS = 1                 # single channel for microphone
+RATE = 44100                 # samples per second
+# create matplotlib figure and axes
+fig, ax = plt.subplots(1, figsize=(15, 7))
+
+# pyaudio class instance
+p = pyaudio.PyAudio()
+
+# stream object to get data from microphone
+stream = p.open(
+    format=FORMAT,
+    channels=CHANNELS,
+    rate=RATE,
+    input=True,
+    output=True,
+    frames_per_buffer=CHUNK
+)
+
+# variable for plotting
+x = np.arange(0, 2 * CHUNK, 2)
+
+# create a line object with random data
+line, = ax.plot(x, np.random.rand(CHUNK), '-', lw=2)
+
+# basic formatting for the axes
+ax.set_title('AUDIO WAVEFORM')
+ax.set_xlabel('samples')
+ax.set_ylabel('volume')
+ax.set_ylim(0, 255)
+ax.set_xlim(0, 2 * CHUNK)
+plt.setp(ax, xticks=[0, CHUNK, 2 * CHUNK], yticks=[0, 128, 255])
+
+# show the plot
+plt.show(block=False)
+
+print('stream started')
+
+# for measuring frame rate
+frame_count = 0
+start_time = time.time()
+
+while True:
+    # 1. Lê os dados binários do microfone
+    data = stream.read(CHUNK, exception_on_overflow=False)  
+    
+    # 2. Converte direto de binário para int16 (muito mais rápido que o struct)
+    data_np = np.frombuffer(data, dtype=np.int16)
+    
+    # 3. Atualiza a linha do gráfico
+    line.set_ydata(data_np)
+    
+    # update figure canvas
+    try:
+        fig.canvas.draw()
+        fig.canvas.flush_events()
+        frame_count += 1
+        
+    except TclError:
+        frame_rate = frame_count / (time.time() - start_time)
+        print('stream stopped')
+        print('average frame rate = {:.0f} FPS'.format(frame_rate))
+        break
